d4ede6365ab1ec2358aeefa194f073a47f417f88
Bekir Niyaz <bagafoot@hotmail.com>
2017-07-25 21:04:43 +0300

    integer default=0

--- GCard/payment/models.py
+++ GCard/payment/models.py
@@ -7,23 +7,24 @@ def card_digit_gen ():
 
 class Card(models.Model):
      digits = models.CharField(max_length=8, default=card_digit_gen, unique=True)
-     balance = models.PositiveSmallIntegerField()
-     def __str__(self):
-         return "Card No: {no} \n Card Balance: {balance}".format(no=self.digits, balance=self.balance)
+     balance = models.PositiveSmallIntegerField(default=0)
+def __str__(self):
+    return "Card No: {no} \n Card Balance: {balance}".format(no=self.digits, balance=self.balance)
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
     desc = models.TextField()
-    price = models.PositiveSmallIntegerField()
+    price = models.PositiveSmallIntegerField(default=0)
     image = models.URLField()
-    def __str__(self):
-        return "Title: {title} \n Description: {desc} \n Price: {price} \n Image Url: {im}".format(title=self.title, desc=self.desc, price=self.price, im=self.image)
+
+def __str__(self):
+    return "Title: {title} \n Description: {desc} \n Price: {price} \n Image Url: {im}".format(title=self.title, desc=self.desc, price=self.price, im=self.image)
 def paymentcard_digit_gen():
     return uuid.uuid4().hex[:10]
 
 class PaymentCard(models.Model):
     digits = models.CharField(max_length=10, default=paymentcard_digit_gen, unique=True)
-    balance = models.PositiveSmallIntegerField()
+    balance = models.PositiveSmallIntegerField(default=0)
     used = models.BooleanField(default=False)
-        def __str__(self):
-        return "Card No: {dig} \n Card Balance: {bal} \n Is Card Used: {used} ".format(dig=self.digits, bal=self.balance, used=self.used)
+def __str__(self):
+    return "Card No: {dig} \n Card Balance: {bal} \n Is Card Used: {used} ".format(dig=self.digits, bal=self.balance, used=self.used)
