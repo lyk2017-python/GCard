9842182b6ecb87e034027df83dc06564ebc7d853
Bekir Niyaz <bagafoot@hotmail.com>
2017-07-25 17:22:16 +0300

    model.py oluşturuldu

---
+++ GCard/payment/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.conf import settings
+
+class Card(models.Model):
+     digits = models.CharField(max_length=8)
+     balance = models.DecimalField(decimal_places=2)
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    desc = models.TextField()
+    price = models.PositiveSmallIntegerField()
+    image = models.URLField()
+
+class PaymentCard(models.Model):
+    digits = models.CharField(max_length=6)
+    balance = models.PositiveSmallIntegerField()
+    used = models.BooleanField()
+
