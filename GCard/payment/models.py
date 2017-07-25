0891df1e27f46eab0cd761ff4c916a38a4fe5cdd
Kemran Dervisov <davrishovkemran@gmail.com>
2017-07-25 19:10:40 +0300

    Card modulunde degisiklikler

--- GCard/payment/models.py
+++ GCard/payment/models.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 class Card(models.Model):
      digits = models.CharField(max_length=8)
-     balance = models.PositiveIntegerField()
+     balance = models.PositiveSmallIntegerField()
 
 
 class Product(models.Model):
