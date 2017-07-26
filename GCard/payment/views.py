f3e25a35db95fc11f104263e1e687c0eb2d4e3a9
Bekir Niyaz <bagafoot@hotmail.com>
2017-07-26 16:10:01 +0300

    conflict

--- GCard/payment/views.py
+++ GCard/payment/views.py
@@ -1,4 +1,7 @@
-from django
+from django.shortcuts import render
+from django.views import generic
+from payment.models import Product,Card,PaymentCard
+
 class card(generic.ListView):
     model = Card
 class pDetail(generic.DetailView):
@@ -7,7 +10,8 @@ class pDetail(generic.DetailView):
 class hView(generic.ListView):
     def get_queryset(self):
         return Product.objects.all()
-class balance(generic.DetailView):
-    model = Card
+class pList(generic.DetailView):
+    def get_queryset(self):
+        return Product.objects.all()
 class HowItWorks(generic.TemplateView):
     template_name = "payment/how_it_works.html"
