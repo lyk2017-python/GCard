8514e1a74bb1963549e97c24753b7e06b3684e93
Batuhan Taşkaya <batuhanosmantaskaya@gmail.com>
2017-07-26 10:37:40 +0300

    Wiki-2 Eklendi

--- GCard/payment/models.py
+++ GCard/payment/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.conf import settings
 import uuid
 
+# Card Digit Generator -> https://github.com/lyk2017-django/GCard/wiki/Models#card_digit_gen-function
 def card_digit_gen ():
     return uuid.uuid4().hex[:8]
 
