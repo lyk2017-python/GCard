1dbea311afba78d90f0bd6404b106c14ccc1f25b
Bekir Niyaz <bagafoot@hotmail.com>
2017-07-26 12:16:31 +0300

    name=product eklendi

--- GCard/payment/urls.py
+++ GCard/payment/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from payment.views import hView, pList, pDetail, card
 
 urlpatterns = [
-    url(r"^$",hView.as_view(), name="home"),
-    url(r"^product/(?P<slug>[A-Za-z0-9-]+)$",pList.as_view()),
-    url(r"^detail/(?P<pk>d+)-(?P<slug>[A-Za-z0-9\-]+)$",pDetail.as_view(), name="product_detail"),
-    url(r"^card/(?P<slug>[A-Za-z0-9\-]+)$",card.as_view(), name="card_detail"), ]
+    url(r"^$", hView.as_view(), name="home"),
+    url(r"^product/(?P<slug>[A-Za-z0-9-]+)$", pList.as_view(), name="product"),
+    url(r"^detail/(?P<pk>d+)-(?P<slug>[A-Za-z0-9\-]+)$", pDetail.as_view(), name="product_detail"),
+    url(r"^card/(?P<slug>[A-Za-z0-9\-]+)$", card.as_view(), name="card_detail"), ]
 
