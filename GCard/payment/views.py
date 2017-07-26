98a7143672ac1f77e7ffeed672c7299f28801db3
BBTaskaya <batuhanosmantaskaya@gmail.com>
2017-07-26 17:16:36 +0300

    Balance Eklendi

--- GCard/payment/views.py
+++ GCard/payment/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from payment.models import Product,Card,PaymentCard
-
+from payment.models import *
 class card(generic.ListView):
     model = Card
 class pDetail(generic.DetailView):
@@ -10,8 +9,7 @@ class pDetail(generic.DetailView):
 class hView(generic.ListView):
     def get_queryset(self):
         return Product.objects.all()
-class pList(generic.DetailView):
-    def get_queryset(self):
-        return Product.objects.all()
+class balance(generic.DetailView):
+    ya = Card
 class HowItWorks(generic.TemplateView):
     template_name = "payment/how_it_works.html"
