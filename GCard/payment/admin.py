41f8ae38ec4ff1cb7b3ca2fc05a9ba527d319d1c
Kemran Dervisov <davrishovkemran@gmail.com>
2017-07-27 11:24:26 +0300

    Admin paneli eklendi ve modeller tanitildi.

--- GCard/payment/admin.py
+++ GCard/payment/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from payment.models import *
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(PaymentCard)
+class PaymentCardAdmin(admin.ModelAdmin):
+    pass
