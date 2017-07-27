fef145f2398b3cb37c195cc5bd5a5998e5fac6ff
BTaskaya <batuhanosmantaskaya@gmail.com>
2017-07-27 13:10:54 +0300

    Brandlar Eklendi / Filterlar Eklendi

--- GCard/payment/admin.py
+++ GCard/payment/admin.py
@@ -3,13 +3,16 @@ from payment.models import *
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price']
-
+    list_display = ['title', 'brand', 'price']
+    search_fields = ['title', 'brand', 'desc', 'price']
+    list_filter = ['brand', 'price']
 @admin.register(Card)
 class CardAdmin(admin.ModelAdmin):
     list_display = ['digits', 'balance']
-
+    search_fields = ['digits']
+    list_filter = ['balance']
 @admin.register(PaymentCard)
 class PaymentCardAdmin(admin.ModelAdmin):
     list_display = ['digits', 'balance', 'used']
-
+    search_fields = ['digits', 'used']
+    list_filter = ['balance', 'used']
--- GCard/payment/models.py
+++ GCard/payment/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     desc = models.TextField()
     price = models.PositiveSmallIntegerField(default=0)
+    brand = models.CharField(max_length=100, default="Unknown")
     image = models.URLField(default="http://lorempixel.com/240/240/technics/")
     def __str__(self):
         return "PK: {pk} \n Title: {title} \n Description: {desc} \n Price: {price} \n Image Url: {im}".format(title=self.title, desc=self.desc, price=self.price, im=self.image, pk=self.pk)
