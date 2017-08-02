f46736866675296601815020a55c5f0bde7b5bd9
BTaskaya <batuhanosmantaskaya@gmail.com>
2017-08-02 10:46:38 +0300

    Temel Giriş Sistem Eklendi

--- GCard/GCard/urls.py
+++ GCard/GCard/urls.py
@@ -17,10 +17,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from payment.urls import urlpatterns as payment_urls
 from django.contrib.auth import views as auth_views
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r"^", include(payment_urls)),
-    url(r"^accounts/login/$", auth_views.LoginView.as_view(), name="login"),
-    url(r"^accounts/logout/$", auth_views.LogoutView.as_view(), name="logout"),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^accounts/logout/$', auth_views.LogoutView.as_view(), name='logout'),
 ]
