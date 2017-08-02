2419c532e7e17cbca0104b136519c6b4c2260566
BTaskaya <batuhanosmantaskaya@gmail.com>
2017-08-02 10:52:57 +0300

    Viewslar Yazıldı

--- GCard/payment/views.py
+++ GCard/payment/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.views import generic
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
@@ -8,6 +12,8 @@ from payment.models import PaymentCard as PaymentCardModel
 from payment.models import Movement as MovementModel
 from django.http import Http404
 from payment.forms import *
+class LoginCreateView(LoginRequiredMixin, generic.CreateView):
+    pass
 
 class pDetail(generic.DetailView):
     def get_queryset(self):
@@ -18,10 +24,13 @@ class hView(generic.ListView):
     template_name = "payment/home.html"
 
 
-class AddBalance(generic.FormView):
+class AddBalance(LoginFormView):
     form_class = AddBalance
     template_name = "payment/card_add_balance_form.html"
     success_url = "/"
+    @method_decorator(login_required)
+    def post(self, request, *a, **kw):
+        return super().post(request, *a, **kw)
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
