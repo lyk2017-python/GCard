21dda03e72471ba822b9ed86ed7edf53fc01a40e
Batuhan Taşkaya <batuhanosmantaskaya@gmail.com>
2017-12-15 19:02:33 +0300

    Update urls.py

--- GCard/payment/urls.py
+++ GCard/payment/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
 from payment.views import *
+
 urlpatterns = [
     url(r"^$", hView.as_view(), name="home"),
     url(r"^detail/(?P<pk>\d+)/$", pDetail.as_view(), name="product_detail"),
     url(r"^transaction/(?P<pk>\d+)/$", ts.as_view(), name="transaction"),
     url(r"^buy/(?P<pk>\d+)/$", BuyProduct.as_view(), name="buy"),
     url(r"^signup/$", RegistrationView.as_view(), name="register"),
-    url(r"^add_balance/$", AddBalance.as_view(), name="add_balance")]
+    url(r"^add_balance/$", AddBalance.as_view(), name="add_balance")
+]
 
