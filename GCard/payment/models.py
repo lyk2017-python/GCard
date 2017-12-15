9228f50c09462520e92c219e34c6cf5ae7f636d8
Batuhan Taşkaya <batuhanosmantaskaya@gmail.com>
2017-12-15 19:07:39 +0300

    Update models.py

--- GCard/payment/models.py
+++ GCard/payment/models.py
@@ -1,22 +1,38 @@
 # General Desc of Models -> https://github.com/lyk2017-django/GCard/wiki/Models#gcard-models-description
+
 from django.db import models
 from django.conf import settings
 import uuid
 import datetime
+
 # Card Digit Generator -> https://github.com/lyk2017-django/GCard/wiki/Models#card_digit_gen-function
+
 def card_digit_gen ():
+    """
+    This function provides us to generate 
+    unique main card id
+    """
     return uuid.uuid4().hex[:8]
 
 # Card Model -> https://github.com/lyk2017-django/GCard/wiki/Models#card-model
+
 class Card(models.Model):
+     """
+     This Model Provides Us To Generate Main Card
+     """
      digits = models.CharField(max_length=8, default=card_digit_gen, unique=True)
      balance = models.PositiveSmallIntegerField(default=0)
      user = models.OneToOneField(settings.AUTH_USER_MODEL, related_name='card')
-     # Product Model -> https://github.com/lyk2017-django/GCard/wiki/Models#cards-str
      def __str__(self):
         return "{no}".format(no=self.digits)
+    
 # Product Model -> https://github.com/lyk2017-django/GCard/wiki/Models#product-model
+
 class Product(models.Model):
+    """
+    This Model Provides Us To Add 
+    A Product To Database.
+    """
     title = models.CharField(max_length=100)
     desc = models.TextField()
     price = models.PositiveSmallIntegerField(default=0)
@@ -24,12 +40,21 @@ class Product(models.Model):
     image = models.URLField(default="http://lorempixel.com/240/240/technics/")
     def __str__(self):
         return "PK: {pk} \n Title: {title} \n Description: {desc} \n Price: {price} \n Image Url: {im}".format(title=self.title, desc=self.desc, price=self.price, im=self.image, pk=self.pk)
+
 # Payment Card Digit Generator ->  https://github.com/lyk2017-django/GCard/wiki/Models#card_digit_gen-function-1
 def paymentcard_digit_gen():
+    """
+    This function provides us to generate 
+    unique disposable card id
+    """
     return uuid.uuid4().hex[:10]
 
 # Payment Card Model -> https://github.com/lyk2017-django/GCard/wiki/Models#card-model-1
 class PaymentCard(models.Model):
+    """
+    This Model Provides Us To Generate 
+    Disposable Card with Generated card ID (10 digits)
+    """
     digits = models.CharField(max_length=10, default=paymentcard_digit_gen, unique=True)
     balance = models.PositiveSmallIntegerField(default=10)
     used = models.BooleanField(default=False)
@@ -37,6 +62,11 @@ class PaymentCard(models.Model):
         return "Card No: {dig} \n Card Balance: {bal} \n Is Card Used: {used} ".format(dig=self.digits, bal=self.balance, used=self.used)
 
 class Movement(models.Model):
+    """
+    This model provides us to keep
+    transactions and show them to the
+    user
+    """
     movement_desc = models.CharField(max_length=100,blank=False,null=False)
     movement_type = models.BooleanField(null=False)
     movement_amount = models.SmallIntegerField(null=False)
